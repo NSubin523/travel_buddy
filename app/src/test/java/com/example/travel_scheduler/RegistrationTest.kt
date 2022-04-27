8b9398216a0901fca13f2d81910a0c8b0042a13b
NSubin523 <neosubin729@gmail.com>
2022-04-26 20:41:32 -0400

    dependency upgraded and added test classes

---
+++ app/src/test/java/com/example/travel_scheduler/RegistrationTest.kt
@@ -0,0 +1,4 @@
+package com.example.travel_scheduler
+
+class RegistrationTest {
+}
---
+++ app/src/test/java/com/example/travel_scheduler/TestDatabase.kt
@@ -0,0 +1,7 @@
+package com.example.travel_scheduler
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class TestDatabase {
+}
